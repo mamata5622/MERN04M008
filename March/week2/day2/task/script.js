043a300c3b93d53e6440d1d8078bdb0b3e0f55dd
mamata5622 <mbehera5622@gmail.com>
2026-03-12 12:33:24 +0530

    task

--- March/week2/day2/task/script.js
+++ March/week2/day2/task/script.js
@@ -2,30 +2,53 @@ console.log("10th of march")
 
 // Capitalize First Letter of Every Word
 
-let str="this is string"
-let cap=()=>{
-    return str.split(""),str.charAt(0),str.toUpperCase(),str.slice(1)
-}
+let arrstr=["i love javascript"];
+arrstr.forEach((word,i)=>{
+    console.log(word)
+})
+let cap=arrstr.map((word)=>{
+    
+})
 
-console.log(cap())
 
 // Find the Most Frequent Element
 
-let arr=[1,4,2,5,1,7,3,1,8,5,4];
+let arr=[1,4,2,5,1,7,3,1,8,56,4];
+
 
 // Group Numbers (Even and Odd)
 
-let arr1=[,1,2,4,7,5,6,8];
+let even=arr.filter((num)=>num%2===0);
+let odd=arr.filter((num)=>num%2!==0);
 
-arr1.forEach()
-
+console.log(even)
+console.log(odd)
+console.log(even.concat(odd))
+ 
 // From an array of numbers, return only numbers greater than 50 using function
+
+let greater=arr.filter((num)=>{
+    return num>50;
+})
+console.log(greater)
 
 // Calculate the total price of products in a array using function.
 
+let products = [
+    { name: "pen", price: 10 },
+    { name: "book", price: 50 },
+    { name: "Bag", price: 100 }
+]
+
+// calculate the total price of all products
+let totalPrice = products.reduce((sum, curr) => sum + curr?.price, 0);
+console.log(totalPrice)
 
 // Replace all spaces in a sentence with hyphens (`-`) in a string.
 
+let str="a b c d e";
+let substitute=str.replaceAll(" ","-")
+console.log(substitute)
 
 // what are the types of function ?
 // what is function currying ?
---
+++ March/week2/day4/script.js
@@ -0,0 +1,2 @@
+let p=document.getElementById("para");
+console.log(p.innerText)
