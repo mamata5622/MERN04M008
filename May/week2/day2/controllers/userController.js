f4fe5e9da66a8909a347ed5922d52ed4b4c644f2
mamata5622 <mbehera5622@gmail.com>
2026-05-13 22:11:26 +0530

    task

--- May/week2/day2/controllers/userController.js
+++ May/week2/day2/controllers/userController.js
@@ -2,13 +2,20 @@ const User = require("../models/userModel");
 
 const getUsers = async (req, res) => {
     try {
+        const { Id } = req.params;
+        const users = await users.findById(Id).select("-password")
+        // res.send({users,message:"success"})
+        if (!users){
+            res.status(404).json({message:"success fetched",user})
+        }
 
+            res.status(200).json({ message: "success" })
     } catch (error) {
-
+        res.status(500).json({ message: "failed" })
     }
 }
 
-const createUser=async(req,res)=>{
+const createUser = async (req, res) => {
     try {
         // bring the data from user
         // get the data from request object
@@ -16,10 +23,10 @@ const createUser=async(req,res)=>{
         // validation on existing 
         // create the user
         // send the response
-        const{fullname,age,email,password}=res.body
-        if(!fullname || !age || !email || !password){
+        const { fullname, age, email, password } = res.body
+        if (!fullname || !age || !email || !password) {
             res.send("Kindly fill the field")
-        }const existUser = await User.findOne({ email });
+        } const existUser = await User.findOne({ email });
         if (existUser) {
             res.send("user already exist")
         } else {
@@ -29,7 +36,53 @@ const createUser=async(req,res)=>{
         }
     } catch (error) {
         console.log(error.message)
-        res.send("failed to create") 
+        res.send("failed to create")
     }
 }
-module.exports={getUsers,createUser}
+
+const updateUser = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const user = req.body
+
+        const existingUser = await User.findById(userId).select("-password");
+
+        if (!existingUser) {
+            res.status(404).json({ message: "user not found" })
+        }
+
+        const updateUser = await User.findByIdAndUpdate(userId, user, { new: true });
+
+        if (!updateUser) {
+            res.status(500).json({ message: "failed to update the user" })
+        }
+        res.status(200).json({ message: "successfully updated", updateUser })
+    } catch (error) {
+
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        const existingUser=await User.findById(userId).select("-password");
+
+        if(!existingUser){
+            res.status(404).json({message:"user not found"})
+        }
+
+        const deletedUser= await User.findByIdAndDelete(userId);
+
+        if(!deleteUser){
+            res.status(500).json({message:"user is not deleted"})
+        }
+
+        res.status(200).json({message:"successfully deleted"})
+
+    } catch (error) {
+        res.status(500).json({ message: "failed to delete" })
+    }
+}
+
+module.exports={getUsers, createUser,  updateUser, deleteUser}
--- May/week2/day2/routes/userRoutes.js
+++ May/week2/day2/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express=require("express")
-const {createUser,getUsers}=require("../controllers/userController")
+const {createUser,getUsers,updateUser,deleteUser}=require("../controllers/userController")
 
 const router=express.Router()
 
-router.get("/",getUsers)
+router.get("/:id",getUsers)
 router.post("/",createUser)
-
+router.put("/:userid",updateUser)
+router.delete("/:userid",deleteUser)
 
 module.exports=router
